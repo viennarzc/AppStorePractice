88681456680b86f6e14f83e859986ee1c302b447
Viennarz Curtiz <viennarzc@gmail.com>
2021-05-14 17:29:50 +0800

    Create Project

---
+++ AppStorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStorePractice
+//
+//  Created by Viennarz Curtiz on 5/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
